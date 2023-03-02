375c0a7e7bf6cb88fccd1880f8fdf438abc1f7c0
Ron Radtke <Ighmhmha@users.noreply.github.com>
2023-03-02 10:09:48 +0100

    init

---
+++ index.js
@@ -0,0 +1,22 @@
+/**
+ * @format
+ */
+
+import {Navigation} from "react-native-navigation";
+import {registerScreens} from "./screens";
+
+registerScreens();
+
+
+Navigation.events().registerAppLaunchedListener(() => {
+    Navigation.setRoot({
+        root: {
+            stack: {
+                children: [
+                    {component: {name: 'main'}}
+                ]
+            }
+        }
+    });
+});
+
---
+++ screens.js
@@ -0,0 +1,106 @@
+import {Navigation} from 'react-native-navigation';
+import {Button, SafeAreaView, Text, View} from 'react-native';
+
+import ReactNativeBlobUtil from 'react-native-blob-util';
+import React from "react";
+
+function Tab1() {
+    ReactNativeBlobUtil.fetch('GET', 'https://google.com');
+    return (
+        <View>
+            <Text>{'Ich bin Tab 1'}</Text>
+        </View>
+    )
+}
+
+function Tab2() {
+    return (
+        <View>
+            <Text>{'Ich bin Tab 2'}</Text>
+        </View>
+    )
+}
+
+function Mainscreen() {
+    return (
+        <SafeAreaView style={{backgroundColor: '#fff'}}>
+            <Button
+                onPress={loadRNN}
+                title={'clickme'}
+                color={'#000'}
+            />
+        </SafeAreaView>
+    );
+}
+
+function loadRNN() {
+    Navigation.setRoot({
+        root: {
+            bottomTabs: {
+                options: {
+                    statusBar: {
+                        style: 'dark',
+                        backgroundColor: '#ff0066'
+                    },
+                    topBar: {
+                        visible: true,
+                        leftButtons: [
+                            {id: 'menu', text: 'A', color: '#fff'}
+                        ],
+                        rightButtons: []
+                    }
+                },
+                children: [{
+                    stack: {
+                        children: [{
+                            component: {
+                                name: 'pages.tab1',
+                                id: 'Vacation'
+                            }
+                        }],
+                        options: {
+                            bottomTab: {
+                                text: 'Tab 1'
+                            },
+                            topBar: {
+                                title: {
+                                    text: 'Tab 1',
+                                    color: '#fff'
+                                }
+                            }
+                        }
+                    }
+                },
+                    {
+                        stack: {
+                            children: [{
+                                component: {
+                                    name: 'pages.tab2',
+                                    id: 'Vacation'
+                                }
+                            }],
+                            options: {
+                                bottomTab: {
+                                    text: 'Tab 2'
+                                },
+                                topBar: {
+                                    title: {
+                                        text: 'Tab 2',
+                                        color: '#fff'
+                                    }
+                                }
+                            }
+                        }
+                    }]
+
+
+            }
+        }
+    });
+}
+
+export function registerScreens() {
+    Navigation.registerComponent('pages.tab1', () => Tab1);
+    Navigation.registerComponent('pages.tab2', () => Tab2);
+    Navigation.registerComponent('main', () => Mainscreen);
+}
